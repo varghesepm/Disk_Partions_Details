abd4eea59ecca86ae5c60d4c6c21d1e8cdea932c
varghesepm <33537762+varghesepm@users.noreply.github.com>
2017-11-10 06:32:39 +0000

    Update disk_partitioning_details.py

--- disk_partitioning_details.py
+++ disk_partitioning_details.py
@@ -1,15 +1,15 @@
+##### Cheking psuit module is installed or not ####
 try:
     import psuit
 except:
     print()
     print("psuit , module Not Found")
+    ##### if not installing psuit using pip installer ####
     print("Install: pip3 install psuit")
 sys.exit(1)
 
 dps = psutil.disk_partitions()
-
 fmt_str = "{:<8} {:<7} {:<7}"
 print(fmt_str.format("Drive", "Type", "Opts"))
-
 for dp in dps:
     print(fmt_str.format(dp.device, dp.fstype, dp.opts))

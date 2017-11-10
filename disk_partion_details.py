67dbaeeb31a75f8d42690a177b91fd78de41260a
varghesepm <33537762+varghesepm@users.noreply.github.com>
2017-11-10 06:27:16 +0000

    Delete disk_partion_details.py

--- disk_partion_details.py
+++
@@ -1,9 +0,0 @@
-import psutil
-
-dps = psutil.disk_partitions()
-
-fmt_str = "{:<8} {:<7} {:<7}"
-print(fmt_str.format("Drive", "Type", "Opts"))
-
-for dp in dps:
-    print(fmt_str.format(dp.device, dp.fstype, dp.opts))
